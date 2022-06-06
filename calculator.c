2613663117d335d5aaf2a39c9f1f6b3ce297b1d6
Abrahamoz_ <100494307+Abrahamosaz@users.noreply.github.com>
2022-06-06 21:33:35 +0100

    A simple calculator

--- calculator.c
+++ calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /**
  * main - Entry Point
  * Description: A simple and functional calculator that adds, subtracts,
@@ -8,29 +9,80 @@
  */
 int main(void)
 {
-    float first_number, second_number;
+    float first_number,second_number;
     char operator_symbol;
-    printf("Enter first number: ");
-    scanf("%f", &first_number);
+    int count;
 
-    printf("Enter operator: ");
-    scanf(" %c", &operator_symbol);
+    count = 0;
 
-    printf("Enter second number: ");
-    scanf("%f", &second_number);
+    while (count <= 3){
+        printf("Enter first number: ");
+        scanf("%f", &first_number);
 
-    if (operator_symbol == '+')
-    {
-        printf("Sum is %0.2f\n", (first_number + second_number));
+        char enter;
+
+        printf("Do you want to enter a second number? Y or N: ");
+        scanf(" %c", &enter);
+        if (enter == 'Y'){
+            printf("Enter the operation? ");
+            scanf(" %c", &operator_symbol);
+
+            printf("Enter second number: ");
+            scanf("%f", &second_number);
+       
+            if (operator_symbol == '+')
+            {
+                printf("Sum is %.2f\n", first_number + second_number);
+            } else if (operator_symbol == '-')
+            {
+                printf("Difference is %.2f\n", first_number - second_number);
+            } else if (operator_symbol == '*')
+            {
+                printf("Product is %.2f\n", first_number * second_number);
+            } else if (operator_symbol == '/')
+            {
+                printf("Division is %.2f\n", first_number / second_number);
+            } else if (operator_symbol == '%') {
+                printf("Reminder is %d\n", (int)first_number % (int)second_number);
+            } else if (operator_symbol == '^'){
+                int square;
+                square = pow(first_number, second_number);
+                printf("The power is %d\n", square);
+            } else {
+                printf("Enter a valid operator!\n");
+            }
+        } else if (enter == 'N'){
+
+            char operator1;
+            printf("Enter the operation to use for the first number? ");
+            scanf(" %c", &operator1);
+            if (operator1 == 'S'){
+                printf("The square of the number is %.2f\n", pow(first_number, 2));
+            } else if (operator1 == 'R'){
+                printf("The square root of the first number is %.2f\n", sqrt(first_number));
+            } else {
+                printf("Enter a valid operator!\n");
+            }
+        } else {
+            printf("Please enter a valid answer!");
+        }
+
+        count++;
+
+        char again;
+
+        if (count == 3){
+            printf("Do you want to use the calculator again?, Y or N: ");
+            scanf(" %c", &again);
+            if (again == 'Y'){
+                count = 0;
+            } else {
+                printf("Thank you for using this calculator!");
+                    break;
+            }
+        }
+
+    
     }
-
-    if (operator_symbol == '-')
-    {
-        printf("Difference is %0.2f\n", (first_number - second_number));
-    }
-
-    if (operator_symbol == '*')
-    {
-        printf("Product is %0.2f\n", (first_number * second_number));
-    }
+    return (0);
 }
